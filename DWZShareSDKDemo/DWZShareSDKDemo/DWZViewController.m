58d1a04253e6e9b6700f0837b9c6913fa1265fa4
lerosua <lerosua@gmail.com>
2014-04-22 09:54:31 +0800

    clean demo

--- DWZShareSDKDemo/DWZShareSDKDemo/DWZViewController.m
+++ DWZShareSDKDemo/DWZShareSDKDemo/DWZViewController.m
@@ -35,47 +35,6 @@
     [DWZShareSDK showDefaultShareWithTitle:@"测试" serviceShareList:nil withViewController:self];
 }
 
-+ (void) showWeiboAuth
-{
-    WBAuthorizeRequest *request = [WBAuthorizeRequest request];
-    request.redirectURI = GESinaWeiboSDKAppUri;
-    request.scope = @"all";
-    request.userInfo = @{@"SSO_From": @"loginFromYinbaShare",
-                         @"Other_Info_1": [NSNumber numberWithInt:123],
-                         @"Other_Info_2": @[@"obj1", @"obj2"],
-                         @"Other_Info_3": @{@"key1": @"obj1", @"key2": @"obj2"}};
-    request.shouldOpenWeiboAppInstallPageIfNotInstalled=NO;
-    [WeiboSDK sendRequest:request];
-}
 
-- (void)request:(WBHttpRequest *)request didFinishLoadingWithResult:(NSString *)result
-{
-    NSString *title = nil;
-    UIAlertView *alert = nil;
-    
-    title = @"收到网络回调";
-    alert = [[UIAlertView alloc] initWithTitle:title
-                                       message:[NSString stringWithFormat:@"%@",result]
-                                      delegate:nil
-                             cancelButtonTitle:@"确定"
-                             otherButtonTitles:nil];
-    [alert show];
-
-}
-
-- (void)request:(WBHttpRequest *)request didFailWithError:(NSError *)error;
-{
-    NSString *title = nil;
-    UIAlertView *alert = nil;
-    
-    title = @"请求异常";
-    alert = [[UIAlertView alloc] initWithTitle:title
-                                       message:[NSString stringWithFormat:@"%@",error]
-                                      delegate:nil
-                             cancelButtonTitle:@"确定"
-                             otherButtonTitles:nil];
-    [alert show];
-
-}
 
 @end
