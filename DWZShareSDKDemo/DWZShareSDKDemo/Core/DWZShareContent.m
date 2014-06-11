71abed13485f66dfdfbc9bc420bf5e860ef91780
lerosua <lerosua@gmail.com>
2014-06-11 17:06:18 +0800

    添加内容字数限制,手工截断

--- DWZShareSDKDemo/DWZShareSDKDemo/Core/DWZShareContent.m
+++ DWZShareSDKDemo/DWZShareSDKDemo/Core/DWZShareContent.m
@@ -7,13 +7,14 @@
 //
 
 #import "DWZShareContent.h"
+static int MaxLength = 130;
 
 @implementation DWZShareContent
 - (instancetype) initWitContent:(NSString *)pContent title:(NSString *)pTitle image:(UIImage *)pImage url:(NSString *)pUrl
 {
     self = [super init];
     if(self){
-        NSInteger length = pContent.length > 180 ? 180 :pContent.length;
+        NSInteger length = pContent.length > MaxLength ? MaxLength :pContent.length;
         self.content = [pContent substringToIndex:length];
         self.title = pTitle;
         self.image = pImage;
